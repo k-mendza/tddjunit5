5eee09604233d85d57ab49c0bbc6c9f03cd259aa
Karol Mendza <karolmendza@gmail.com>
2019-08-23 22:26:19 +0200

    Nested assertAll example

---
+++ src/test/java/model/OwnerTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        //given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        //when
+        owner.setCity("City");
+        owner.setTelephone("111222333");
+        //then
+        assertAll("Owner Class test",
+                () -> assertAll("Person properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "Wrong firstName value"),
+                        () -> assertEquals("Buck", owner.getLastName(), "Wrong lastName value")),
+                () -> assertAll("Owner properties",
+                        () -> assertEquals("City", owner.getCity(), "Wrong city value"),
+                        () -> assertEquals("111222333", owner.getTelephone(), "Wrong telephone value")
+                ));
+    }
+
+}
